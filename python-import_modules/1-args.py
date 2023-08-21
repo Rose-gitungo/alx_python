ccaf7840af6fcd297cde6a7979e6c5aee498fed3
gitungo rose <rgitungo@kabarak.ac.ke>
2023-08-22 00:31:36 +0300

    arguments

---
+++ python-import_modules/1-args.py
@@ -0,0 +1,17 @@
+if len(argv)==1:
+    x='argument.'
+else:
+    x='arguments:'
+print(len(argv)+'x'+"\n")
+
+def numb():
+    for i in range(len(argv)):
+        n=0
+        print('%n+1:'+ i +"\n")
+
+
+
+
+if __name__ =="__main__":
+    import sys
+    sys.exit()
