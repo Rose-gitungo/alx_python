b0c2a9e5d3d2d15fef01df31d5a80a3705262ab5
gitungo rose <rgitungo@kabarak.ac.ke>
2023-08-22 19:30:15 +0300

    raising exceptions

--- python-import_modules/5-raise_exceptions_msg.py
+++ python-import_modules/5-raise_exceptions_msg.py
@@ -1,7 +1,7 @@
 def raise_exception_msg(message=""):
     try:
-        raise_exception_msg("C is fun")
-    except NameError as ne:
-        print(ne)
+        message="C is fun"
+    except NameError as message:
+        print(message)
 
     return
