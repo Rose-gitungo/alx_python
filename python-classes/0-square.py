8ee10cb0515582dde9e6caa66a5df596fc8a6401
gitungo rose <rgitungo@kabarak.ac.ke>
2023-08-30 16:49:18 +0300

    gogo

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -2,6 +2,7 @@
 class Square():
     """"python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
     size=""
-    def __init__(self,size):   
+    def __init__(self,size):  
+        """python3 -c 'print(__import__("my_module").my_function.__doc__)'""" 
+        """python3 -c 'print(__import__("my_module").MyClass.my_function.__doc__)'"""
         self.__size=size
-
