8bbd81eed89861005a9b2eb231126eb1ecb95754
gitungo rose <rgitungo@kabarak.ac.ke>
2023-08-30 16:14:18 +0300

    corr 0-square

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -1,7 +1,11 @@
 """"python3 -c 'print(__import__("my_module").__doc__)'"""
-class Square:
+class Square():
     """"python3 -c 'print(__import__("my_module").MyClass.__doc__)'"""
-
-    def __init__(self) -> None:
-        self.__size = size
+    size=""
+    def __init__(self,size) -> None:   
+        self.size=size
+        
+my_square = Square(89)
+print(type(my_square))
+print(my_square.__dict__)
 
