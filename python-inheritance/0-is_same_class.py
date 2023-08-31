c06cd109d17eec7a21b9b93077df3a9449da2c25
gitungo rose <rgitungo@kabarak.ac.ke>
2023-08-31 13:03:54 +0300

    0-main

--- python-inheritance/0-is_same_class.py
+++ python-inheritance/0-is_same_class.py
@@ -1,5 +1,14 @@
 def is_same_class(obj,a_class):
-    """a function"""
+    """
+    Checks if the object is exactly an instance of the specified class.
+
+    Args:
+        obj:The object to check.
+        a_class: The class to compre against.
+
+    Returns:
+    bool: True if obj is an instance of a_class,False otherwise.
+    """
     if issubclass(a_class,int)== True:
          return True
     else:
