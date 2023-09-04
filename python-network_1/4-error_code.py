edfacbefc37d5bf0e4132aa1108da78bd86b4e1e
gitungo rose <rgitungo@kabarak.ac.ke>
2023-09-04 15:59:04 +0300

    commit

---
+++ python-network_1/4-error_code.py
@@ -0,0 +1,14 @@
+"""
+Takes in a url and sends request to url and display response
+"""
+
+import requests
+import sys
+
+if __name__=="__main__":
+    url = sys.argv[1]
+    r=requests.get(url)
+    if (r.status_code) >= 400:
+        print("Error code:",r.status_code)
+        
+
---
+++ python-network_1/5-json_api.py
@@ -0,0 +1,26 @@
+"""
+Takes a letter and send a post request to a specified url with letter as paramater
+"""
+
+import sys
+import requests
+
+if __name__=="__main__":
+    if len(sys.argv)<2:
+        q=""
+    else:
+        q= sys.argv[1]
+
+        url= 'http://0.0.0.0:5000/search_user'
+        data ={"q":q}
+        
+    r=requests.post(url,params = data)
+
+    try:  
+        json_data =r.json()
+        if json_data:
+            print('[{}] {}'.format(json_data["id"],json_data["name"]))
+        else:
+            print("No result")
+    except ValueError:
+        print("Not a valid JSON")
