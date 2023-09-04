d5df2d1b281c65a25385905c5810170070fc4aa0
gitungo rose <rgitungo@kabarak.ac.ke>
2023-09-04 15:42:40 +0300

    commit

--- python-network_1/2-post_email.py
+++ python-network_1/2-post_email.py
@@ -4,11 +4,9 @@ Taking a url and an email address and sending a post request to the url.
 import requests
 import sys
 
-
-
 if __name__ =="__main__":
     url = sys.argv[1]
     email = sys.argv[2]
     payload ={'email':email}
     r=requests.post('url',params=payload)
-    print(r)
+    print(r.text)
