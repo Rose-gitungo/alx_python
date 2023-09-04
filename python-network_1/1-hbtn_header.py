e595bd661b11fe6b9263035648e5430c60ea1c0c
gitungo rose <rgitungo@kabarak.ac.ke>
2023-09-04 15:35:39 +0300

    commit

--- python-network_1/1-hbtn_header.py
+++ python-network_1/1-hbtn_header.py
@@ -1,9 +1,12 @@
-"""python3 -c 'print(__import__("my_module").__doc__)'"""
+"""
+Sends request to urla dn displays varibale X-Requests-Id.
+"""
 import requests
 import sys
 
-url=sys.stdin
+
 if __name__=="__main__":
+    url=sys.argv[1]
     r=requests.get(url)
     x_id=r.headers.get("X-Request-Id")
     print(x_id)
